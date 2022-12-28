8d6081354bb74db56a2dbc5264dd7159ba017c4d
Edoardo Arnaudo <edoardo.arn@gmail.com>
2022-12-28 10:26:46 +0000

    🔨 Remove unecessary comments

--- tools/test.sh
+++ tools/test.sh
@@ -3,5 +3,4 @@
 set -e
 set -x
 
-# Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
 pytest --cov=argdantic --cov-report=xml -o console_output_style=progress
