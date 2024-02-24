188827574d8afdd65b51427b232ceb6a38c80832
Edoardo Arnaudo <edoardo.arn@gmail.com>
2024-02-24 17:44:10 +0100

    🔖 Release version 1.0.1

--- argdantic/__init__.py
+++ argdantic/__init__.py
@@ -1,9 +1,10 @@
 """Typed command line interfaces with argparse and pydantic
 """
+
 from argdantic.core import ArgParser
 from argdantic.fields import ArgField
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = [
     "ArgParser",
     "ArgField",
